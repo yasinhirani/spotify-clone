77cd9e8e66aea74978edf17c72cc4a05e90f90a1
Yasin Hirani <yasin.hirani@1rivet.com>
2025-03-18 16:43:10 +0530

    fix: random song playing

--- src/shared/utilities/service/music.service.ts
+++ src/shared/utilities/service/music.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createApi } from "@reduxjs/toolkit/query/react";
 import axiosBaseQuery from "../../../core/utilities/service/axios.service";
+import axios from "axios";
 
 const musicApi = createApi({
   reducerPath: "MusicApi",
@@ -14,9 +15,25 @@ const musicApi = createApi({
         url: `/api/search/song?query=${musicName}`,
         method: "GET",
       }),
-    })
+    }),
   }),
 });
 
 export const { useGetMusicDataQuery } = musicApi;
 export default musicApi;
+
+export const fetchAlternateMusicUrl = async (songId: string) => {
+  const res = await axios.post(
+    "https://spotify-downloader-api-download-track-album-playlists.p.rapidapi.com/download_song",
+    { song_id: songId },
+    {
+      headers: {
+        "x-rapidapi-key": import.meta.env.VITE_RAPID_API_KEY,
+        "x-rapidapi-host":
+        import.meta.env.VITE_RAPID_API_HOST,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  return res.data;
+};
